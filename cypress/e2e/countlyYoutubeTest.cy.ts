2003b097ea7351b1d8abe50611f70974d6865e0e
ilyas <ilyas.uckac@abeyis.com>
2024-01-04 13:31:01 +0300

    youtubeTask

---
+++ cypress/e2e/countlyYoutubeTest.cy.ts
@@ -0,0 +1,75 @@
+
+const countly = require("../fixtures/countly.json")
+import { youtubePage } from "../support/youTubeSelector";
+describe('Countly youTube Test', () => {
+
+    beforeEach(() => {
+        // Go to URL select Countly video
+        cy.visit(countly.url)  
+        cy.sendKeys(youtubePage.searchBox, countly.Countly) 
+        cy.wait(8000)
+        cy.clickk(youtubePage.searchButton) 
+        cy.clickk(youtubePage.countlyVideo) 
+
+    });
+
+    it('TC01 Verify the video URL', () => {
+        cy.assertUrl(countly.pathParam) 
+    });
+
+    it('TC02 Verify the video title name', () => {
+        cy.wait(6000)
+        cy.assertGetByText(youtubePage.videoName, countly.videoNameText) 
+        
+    });
+
+    it('TC03 Verify the video upload date', () => {
+        cy.clickk(youtubePage.seeMoreButton)
+        cy.assertGetByText(youtubePage.uploadDate, countly.uploadDateText) 
+        
+    });
+
+    it('TC04 Verify the video owner name', () => {
+        cy.assertGetByText(youtubePage.videoOwnerName, countly.videoOwnerText) 
+
+    });
+
+    it('TC05 Verify the number of like more than 10', () => {
+        cy.likeButtonAssert()
+        
+    });
+
+    it('TC06 Verify the  number of subscribers is less than 1000', () => {
+        cy.wait(5000)
+        cy.subscribersAssert()
+
+    });
+
+    it('TC07 Verify  advertisement content countdown', () => {
+        cy.wait(3000)
+        cy.adsCountAssert()
+        cy.visibleByText('Atla')
+        cy.clickByText('Atla')
+
+    });
+
+    it('TC08 Verify the video stopped', () => {
+        cy.clickk(youtubePage.pauseButton)
+        cy.contains('Oynat (k)').should('exist')
+
+    });
+
+    it('TC09 Verify the page header is fixed at the top', () => {
+        cy.scrollTo('bottom');
+        cy.visible(youtubePage.youtubeLogo)
+        
+    });
+
+    it.only('TC10 Verify the selected random video name', () => {
+        cy.wait(3000)
+        cy.randomVideoSelect()
+        cy.wait(3000)
+        cy.randomVideoAssert()
+
+    });
+});
---
+++ cypress/support/generalCommands.ts
@@ -0,0 +1,94 @@
+import { youtubePage } from "../support/youTubeSelector";
+export { }
+declare global {
+    namespace Cypress {
+        interface Chainable {
+            clickk(selector: string): Chainable<void>
+            clickByText(selector: string): Chainable<void>
+            visibleByText(text: string): Chainable<void>
+            assertUrl(pathParam: string): Chainable<void>
+            sendKeys(selector,data: string): Chainable<void>
+            assertGetByText(selector,text: string): Chainable<void>
+            likeButtonAssert():Chainable<void>
+            subscribersAssert():Chainable<void>
+            adsCountAssert():Chainable<void>
+            randomVideoSelect():Chainable<void>
+            randomVideoAssert():Chainable<void>
+            visible(selector:string): Chainable<void>
+        }
+    }
+}
+Cypress.Commands.add('clickk', (selector) => {
+    cy.get(selector)
+        .should('be.visible')
+        .click()
+})
+Cypress.Commands.add('clickByText', (text) => {
+    cy.contains(text)
+        .click({ force: true })
+})
+Cypress.Commands.add("visibleByText",(text)=>{
+    cy.contains(text,{timeout:25000})
+    .should('be.visible')
+})
+Cypress.Commands.add('assertUrl',(pathParam)=>{
+    cy.url().should('include',pathParam)      
+})
+
+Cypress.Commands.add('sendKeys',(selector,data)=>{
+    cy.get(selector).should('exist').should('be.visible').type(data).should('have.value',data)
+})
+
+Cypress.Commands.add('assertGetByText',(selector,text)=>{
+    cy.get(selector).should('contain',text)
+})
+Cypress.Commands.add('likeButtonAssert',()=>{
+    cy.get(youtubePage.likeButton)
+            .first()
+            .invoke('text').then((textContent) => {
+                
+                const numberValue = parseInt(textContent);
+                
+                cy.wrap(numberValue).should('be.gt', 10);
+            });
+})
+Cypress.Commands.add('subscribersAssert',()=>{
+    cy.get(youtubePage.subscribers)
+            .invoke('text').then((textContent) => {
+                
+                const numberValue = parseInt(textContent);
+                
+                cy.wrap(numberValue).should('be.lt', 1000);
+            });
+})
+Cypress.Commands.add('adsCountAssert',()=>{
+    cy.get(youtubePage.adsCount).then(($currentTime) => {
+
+        const currentTime = parseFloat($currentTime.text().replace(':', '.'));
+        expect(currentTime).to.be.closeTo(0, 2); 
+    });
+})
+let elementText;
+Cypress.Commands.add('randomVideoSelect',()=>{
+    
+        cy.get(':nth-child(18) > #dismissible > .details > .metadata > .yt-simple-endpoint > h3.style-scope > #video-title')
+            .invoke('text')
+            .then((text) => {
+                elementText = text.trim();
+                cy.log('Alinan text:', elementText);
+            });
+        cy.get(':nth-child(18) > #dismissible > .details > .metadata > .yt-simple-endpoint > h3.style-scope > #video-title').click()
+})
+Cypress.Commands.add('randomVideoAssert',()=>{
+    let element2Text
+        cy.get('#above-the-fold > :nth-child(1) > h1.style-scope > .style-scope')
+            .invoke('text')
+            .then((text) => {
+                element2Text = text.trim();
+                cy.log('Alinan text:', element2Text);
+                expect(elementText).to.eq(element2Text)
+            });
+})
+Cypress.Commands.add('visible',(selector)=>{
+    cy.get(selector).should('be.visible')
+})
